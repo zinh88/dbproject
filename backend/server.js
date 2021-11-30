655824955863d429219f4acdaaa9a278d44a8323
zinh88 <zaink9898@gmail.com>
2021-12-01 04:30:59 +0500

    format server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const bcrypt = require('bcrypt')
 
 const app = express()
 
@@ -17,12 +16,3 @@ app.use('/api/user', authRoute);
 app.listen(5000, () => {
     console.log('Server is Running')
 });
-
-const firstuser = async () => {
-    const pass = '1234';
-    const salt = await bcrypt.genSalt();
-    const hashed = await bcrypt.hash(pass, salt);
-    console.log(hashed);
-
-}
-firstuser()
