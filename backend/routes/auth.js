713c7e7217fdff15d3adb65ef07d898beec49b18
zinh88 <zaink9898@gmail.com>
2021-12-02 01:06:21 +0500

    new env

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,20 +1,31 @@
+const pool = require('../config/db');
 const { Router } = require('express');
 const controller = require('../controllers/auth');
-const pool = require('../config/db');
 
 const router = Router();
 
 router.post('/signup', (req, res) => {    
     res.send(`user ${req.body.mail} already exists`)
-})
+});
+
+
+
 router.post('/login', (req, res) => {
     const email = req.body.mail;
     const pass = req.body.pass;
-    pool.query('SELECT * FROM members', (err, result) => {
-        console.log(result);
-    })
+
+    pool.query(`SELECT * FROM members WHERE email=\'${email}\'`, (err, result) => {
+        if(err) console.log(err);
+        if (result.rowCount === 1) 
+            console.log('success')
+        else console.log('no user')
+    });
+
+
     
     console.log(req.body.mail, req.body.pass)
-})
+});
+
+
 
 module.exports = router;
